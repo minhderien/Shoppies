92aa3c007193fcba3fc9516bc6a3f0b311886171
Minh Nguyen <minhtan16@hotmail.com>
2020-12-23 15:09:35 -0500

    fix: https on heroku

--- src/service/omdb-base-config.js
+++ src/service/omdb-base-config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseDomain = 'http://www.omdbapi.com' ;
+const baseDomain = 'https://www.omdbapi.com' ;
 const baseURL = `${baseDomain}`;
 
 export default axios.create({
